b56a4cc65685b94716a1e45bc1bccccc79649cdc
Peter Laudy <peter.laudy@oce.com>
2019-04-15 11:35:35 +0200

    The polyline directive better handles the strings coming from the Inputs.

--- src/app/directives/polyline.directive.ts
+++ src/app/directives/polyline.directive.ts
@@ -13,8 +13,8 @@ import { SvgLocation } from '../classes/location.model';
  */
 export class PolylineDirective implements AfterViewInit {
     @Input('appPolyline') coordinates: SvgLocation[];
-    @Input('first') first: number;
-    @Input('last') last: number;
+    @Input() first: string;
+    @Input() last: string;
 
     constructor(private el: ElementRef) { }
 
@@ -23,29 +23,29 @@ export class PolylineDirective implements AfterViewInit {
      */
     ngAfterViewInit(): void {
         // We get the 3 attributes which are the coordinates to get the points from ...
-        if (null == this.coordinates) {
+        if (this.coordinates) {
             return;
         }
+
         // ... the first index in the array (or 0 if not specified) ...
-        if (null == this.first) {
-            this.first = 0;
-        } else {
+        let first = 0;
+        if (this.first) {
             // This is comming from an attribute, so it is a string.
-            this.first = Math.min(+this.first, this.coordinates.length - 1);
+            first = Math.min(+this.first, this.coordinates.length - 1);
         }
+
         // ... and the last index in the array (or the last one is not specified).
-        if (null == this.last) {
-            this.last = this.coordinates.length - 1;
-        } else {
+        let last = this.coordinates.length - 1;
+        if (this.last) {
             // This is comming from an attribute, so it is a string.
-            this.last = Math.min(+this.last, this.coordinates.length - 1);
+            last = Math.min(+this.last, this.coordinates.length - 1);
         }
 
         // We don't set the 'points' attribute if there are no coordinates to show.
-        if (this.first < this.last) {
-            let points: string = '';
+        if (first < last) {
+            let points = '';
             // A simple for loop does the trick.
-            for (let i: number = this.first ; i <= this.last; i++) {
+            for (let i = first ; i <= last; i++) {
                 points += `${this.coordinates[i].x},${this.coordinates[i].y} `;
             }
             // Finally we set the attribute on the polyline element.
